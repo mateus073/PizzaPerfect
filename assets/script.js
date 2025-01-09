6e15b3e4c51c87b9f443577c76a8b406fae19310
Mateus Marques da Silva <mateus.marques.da.silva@gmail.com>
2025-01-09 18:32:49 -0300

    agora exibe ingredientes

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 /*VARIAVEIS */
 let btnproximo1 = document.querySelector('#proximo')
 let opcoes = document.querySelectorAll('.opc');
-let listaOpc = document.querySelector('.opcoes');
-let opcoes2 = document.querySelector('.opcoes2');
+let divOpc1 = document.querySelector('.opc11')
+let divOpc2 = document.querySelector('.opc22')
+let listaOpc = document.querySelector('.ol2');
+let aperto = true
+let igdeEscolhidos = []
 
 
 
@@ -12,32 +15,61 @@ let opcoes2 = document.querySelector('.opcoes2');
 opcoes.forEach(opc => {
     opc.addEventListener('click', (e) => {
         let ctgrEscolhida = e.target.getAttribute('data-op'); // Se o atributo "data-h" estiver no HTML
+        // deixa a categoria na cor vermelha e passa o nome do objeto que irei trablhar 
+        if(aperto) {
+        e.target.style.backgroundColor = "#a12630"
         prencheTela(ctgrEscolhida);
+        aperto = false
+        }
     });
 });
 
 
 
+
+
 /*FUNCOES  */
+
 // funcao que recebe o data op e prenche e tela com os ingredientes da categoria
 //dataop e o indice do objeto escolhido 
 function prencheTela(dataop) {
     if (dataop) {
         btnproximo1.addEventListener('click', () => {
-            window.location.href = 'escolhaOpc2.html'
+            divOpc1.style.display = "none" 
+            divOpc2.style.display = "flex" 
         })
 
         // obtem os ingredites gerais
         let ingredientesG = cardapio[dataop].ingredientesGerais
-        console.log(ingredientesG)
-          // Itera sobre o array de ingredientes
-          for (let x = 0; x < ingredientesG.length; x++) {
+
+        // Itera sobre o array de ingredientes
+        for (let x = 0; x < ingredientesG.length; x++) {
             let texto = ingredientesG[x]; // Obtém o texto do ingrediente
-            let newLi = document.createElement("li"); // Cria um novo elemento <li>
-            newLi.className = "opc2"
+            let newLi = document.createElement('li'); // Cria um novo elemento <li>
+            newLi.classList.add('opc2')
             newLi.innerText = texto; // Adiciona o texto ao <li>
 
-            opcoes2.appendChild(newLi); // Adiciona o <li> à lista
+            listaOpc.appendChild(newLi); // Adiciona o <li> à lista
         }
+        escolhePizza(ingredientesG)
     }
+}
+
+
+
+// funcao reposavel por pegar todas as pizzas  e acessar seu array de ingredientes pra comparar com o os ingredientes escolhidos
+function escolhePizza(ingrestesg) {
+    document.querySelectorAll('.opc2').forEach(opcVez => {
+        opcVez.addEventListener('click', (e) => {
+            let ingrdClicados = e.target.innerText
+            if(igdeEscolhidos.length <= 3) {
+                igdeEscolhidos.push(ingrdClicados)
+                console.log(igdeEscolhidos)
+                e.target.style.backgroundColor = "#a12630"
+            }
+            else{
+                
+            }
+        })
+    })
 }
