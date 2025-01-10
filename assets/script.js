d2ec5406ff24db11a94e385b0bbdb3054566eee4
Mateus Marques da Silva <mateus.marques.da.silva@gmail.com>
2025-01-10 14:38:52 -0300

    antes de tentar os varios metodos pra filtrar a pizza

--- assets/script.js
+++ assets/script.js
@@ -16,10 +16,10 @@ opcoes.forEach(opc => {
     opc.addEventListener('click', (e) => {
         let ctgrEscolhida = e.target.getAttribute('data-op'); // Se o atributo "data-h" estiver no HTML
         // deixa a categoria na cor vermelha e passa o nome do objeto que irei trablhar 
-        if(aperto) {
-        e.target.style.backgroundColor = "#a12630"
-        prencheTela(ctgrEscolhida);
-        aperto = false
+        if (aperto) {
+            e.target.style.backgroundColor = "#a12630"
+            prencheTela(ctgrEscolhida);
+            aperto = false
         }
     });
 });
@@ -30,17 +30,17 @@ opcoes.forEach(opc => {
 
 /*FUNCOES  */
 
-// funcao que recebe o data op e prenche e tela com os ingredientes da categoria
-//dataop e o indice do objeto escolhido 
+// funcao que exibe a proxima tela e recebe o data-op(e o indice do objt escolhido) e prenche e tela com os ingredientes da categoria
 function prencheTela(dataop) {
     if (dataop) {
         btnproximo1.addEventListener('click', () => {
-            divOpc1.style.display = "none" 
-            divOpc2.style.display = "flex" 
+            divOpc1.style.display = "none"
+            divOpc2.style.display = "flex"
         })
 
         // obtem os ingredites gerais
         let ingredientesG = cardapio[dataop].ingredientesGerais
+        let categoriaEscolhida = cardapio[dataop]
 
         // Itera sobre o array de ingredientes
         for (let x = 0; x < ingredientesG.length; x++) {
@@ -51,25 +51,49 @@ function prencheTela(dataop) {
 
             listaOpc.appendChild(newLi); // Adiciona o <li> à lista
         }
-        escolhePizza(ingredientesG)
+        escolhePizza(categoriaEscolhida)
     }
 }
 
 
 
-// funcao reposavel por pegar todas as pizzas  e acessar seu array de ingredientes pra comparar com o os ingredientes escolhidos
-function escolhePizza(ingrestesg) {
+// funcao reposavel por pegar as pizzas que o usuario clicou e mudar sua cor 
+function escolhePizza(categoriaEscolhida) {
     document.querySelectorAll('.opc2').forEach(opcVez => {
         opcVez.addEventListener('click', (e) => {
             let ingrdClicados = e.target.innerText
-            if(igdeEscolhidos.length <= 3) {
+
+            if (igdeEscolhidos.length <= 2) {
                 igdeEscolhidos.push(ingrdClicados)
-                console.log(igdeEscolhidos)
                 e.target.style.backgroundColor = "#a12630"
+                console.log(igdeEscolhidos)
             }
-            else{
-                
+            else if (ingrdClicados.length >= 2) {
+                retornaPizza(igdeEscolhidos, categoriaEscolhida)
             }
         })
     })
 }
+
+
+// funcao pra retorna a pizza que mais se assemelha aos ingredientes escolhidos 
+function retornaPizza(igdeEscolhidos, cardapEscolhido) {
+    let arrayObjCardap = cardapEscolhido.pizzas
+    let igdArray = igdeEscolhidos
+    console.log(igdArray)
+    console.log(arrayObjCardap)
+    
+    console.log(Array.isArray(igdArray) )
+
+    return arrayObjCardap.map(objVez => {
+        // conta quantos igred cada pizza tem semelhante aos escolhidos 
+        const contador = objVez.ingredientes.filter(item => {
+            return igdArray.includes(item)
+        }).length;
+        return { nome: objVez.nome, quantidadeIguais: contador }
+        // return console.log({ nome: objVez.nome, quantidadeIguais: contador })
+    })
+    .reduce((maior, atual) => atual.quantidadeIguais > maior.quantidadeIguais ? atual : maior
+    )}
+
+
